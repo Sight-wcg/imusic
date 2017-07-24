2b6b25a6c00d2e4d0a05e69c321a41aba32bdc40
ilpan <pna110@126.com>
2017-07-24 15:41:38 +0800

    实现用户关注用户

--- src/com/imusic/dao/UserFocusUserDAO.java
+++ src/com/imusic/dao/UserFocusUserDAO.java
@@ -1,7 +1,20 @@
 package com.imusic.dao;
 
+import com.imusic.bean.User;
+
+import java.util.List;
+
 /**
  * Created by pan on 17-7-24.
  */
-public class UserFocusUserDAO {
+public interface UserFocusUserDAO {
+
+    // fromUserID 添加关注 toUserID
+    public void addFocus(int fromUserID, int toUserID);
+
+    // fromUserID 删除关注 toUserID
+    public void delFocus(int fromUserID, int toUserID);
+
+    // 查找 fromUserID 的所有关注（用于显示）
+    public List<User> findAllUser(int fromUserID);
 }
--- src/com/imusic/daoImpl/UserFocusUserDAOImpl.java
+++ src/com/imusic/daoImpl/UserFocusUserDAOImpl.java
@@ -1,7 +1,61 @@
 package com.imusic.daoImpl;
 
+import com.imusic.bean.User;
+import com.imusic.dao.UserFocusUserDAO;
+import com.imusic.util.DBConnection;
+import com.imusic.util.DBOperation;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by pan on 17-7-24.
+ *
+ * fromUserID and toUserID 可有提高者自己 get
+ * eg: fromUserID = fromUser.getUserID();
+ *     toUserID = toUser.getUserID();
  */
-public class UserFocusUserDAOImpl {
+public class UserFocusUserDAOImpl implements UserFocusUserDAO {
+    @Override
+    public void addFocus(int fromUserID, int toUserID) {
+        String addFocusSQL = "insert into userFocusUser" +
+                "(fromUserID, toUserID) values(?, ?)";
+        DBOperation.dateOperate(fromUserID, toUserID, addFocusSQL);
+    }
+
+    @Override
+    public void delFocus(int fromUserID, int toUserID) {
+        String delFocusSQL = "delete from userFocusUser " +
+                "where fromUserID = ? and toUserID = ?";
+        DBOperation.dateOperate(fromUserID, toUserID, delFocusSQL);
+    }
+
+    @Override
+    public List<User> findAllUser(int fromUserID) {
+        Connection conn = DBConnection.getConnection();
+        PreparedStatement pstmt;
+        String findAllUserSQL = "select * from user where userID in " +
+                "(select toUserID form userFocusUser where fromUserID = ?";
+        ResultSet rs;
+        List<User> userList;
+        try {
+            pstmt = conn.prepareStatement(findAllUserSQL);
+            pstmt.setInt(1, fromUserID);
+            rs = pstmt.executeQuery();
+            userList = new ArrayList<>();
+            while (rs.next()) {
+                userList.add(new User(rs.getInt(1), rs.getString(2), rs.getString(3),
+                        rs.getString(4), rs.getInt(5), rs.getDate(6), rs.getDate(7),
+                        rs.getString(8), rs.getString(9), rs.getString(10), rs.getDate(11)));
+            }
+            return userList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
